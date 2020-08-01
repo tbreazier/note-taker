1f9fc77e7cb1bc361e7bc47e119fc00b8ca6be81
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-31 21:22:11 -0500

    Heroku setup

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 
+const PORT = process.env.PORT || 3001;
+
 const app = express();
 
-const { notes } = require('./db/db.json')
+const notes = require('./db/db')
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.get('/api/notes', (req, res) => {
+    res.send(notes);
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
