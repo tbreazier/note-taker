a70c4844371ae28c4859272af73876a7f30cfb9e
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-07-31 21:40:38 -0500

    added post route

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const notes = require('./db/db')
 
 app.get('/api/notes', (req, res) => {
     res.send(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+    res.json(req.body);
 });
 
 app.listen(PORT, () => {
