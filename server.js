520a7fe6726d72e0feb53d9b9e0932c2874fa1e3
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-02 10:27:46 -0500

    separating routes

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
+//Requires
 const express = require('express');
+const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
+//Assigns 3001 as port, or another or another if unavailable
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-const notes = require('./db/db')
-
-app.get('/api/notes', (req, res) => {
-    res.send(notes);
-});
-
-app.post('/api/notes', (req, res) => {
-    res.json(req.body);
-});
-
-function createNewNote(body, notesArray) {
-    console.log(body);
-
-    return body;
-}
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
