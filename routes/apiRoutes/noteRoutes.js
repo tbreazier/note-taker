a57cb6911717af1de49bacb7b1677cf1b891603d
tbreazier <64601341+tbreazier@users.noreply.github.com>
2020-08-02 12:05:47 -0500

    deploy to heroku

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,16 @@
+//Variables
 const router = require('express').Router();
 const path = require('path');
 const shortid = require('shortid');
 const { createNote } = require('../../lib/notes')
 let notesArray = require('../../db/db.json');
 
-
+//Route to DB
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../../db/db.json'))
 });
 
+//Write to DB
 router.post('/', (req, res) => {
     req.body.id = shortid.generate()
     const note = createNote(req.body)
--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,10 +1,13 @@
+//Variables
 const path = require("path");
 const router = require("express").Router();
 
+//Returns Homepage
 router.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/index.html"));
 });
-  
+
+//Returns Notes Page
   router.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../../public/notes.html"));
 });
